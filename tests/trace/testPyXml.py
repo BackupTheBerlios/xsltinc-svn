6ef9290c47cf3fe92215d68f71459ab18b87f490
mikael <mikael@093d897c-bf01-0410-95d3-d7bdf09db677>
2005-11-08 15:58:11 +0000

    uhm

--- tests/trace/testPyXml.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#
-
-from xml.xslt.Processor import Processor
-
-xsltproc = Processor()
-
-xsltproc.appendStylesheetUri("file:///home/t0rt00se/Travail/SILR3/pTrans/testset/persons_to_xhtml_list.xsl")
-html = xsltproc.run(DefaultFactory.fromUri("persons.xml"))
